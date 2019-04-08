a1c20c519c7c72320e1fa24955e049e930b306f7
noboru-i <ishikura.noboru@gmail.com>
2019-04-09 00:08:26 +0900

    create sample ui

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/material.dart';
 import 'dart:async';
 
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_gps_distance/flutter_gps_distance.dart';
 
@@ -13,6 +13,14 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   String _platformVersion = 'Unknown';
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  String startLatitude;
+  String startLongitude;
+  String endLatitude;
+  String endLongitude;
+
+  double distance = 0;
 
   @override
   void initState() {
@@ -47,10 +55,72 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+        body: Container(
+          margin: new EdgeInsets.all(20.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'start latitude',
+                    border: UnderlineInputBorder(),
+                  ),
+                  onSaved: (String value) {
+                    startLatitude = value;
+                  },
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'start longitude',
+                    border: UnderlineInputBorder(),
+                  ),
+                  onSaved: (String value) {
+                    startLongitude = value;
+                  },
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'end latitude',
+                    border: UnderlineInputBorder(),
+                  ),
+                  onSaved: (String value) {
+                    endLatitude = value;
+                  },
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'end longitude',
+                    border: UnderlineInputBorder(),
+                  ),
+                  onSaved: (String value) {
+                    endLongitude = value;
+                  },
+                ),
+                RaisedButton(
+                  child: const Text('SUBMIT'),
+                  onPressed: _handleSubmitted,
+                ),
+                Text("distance: " + distance.toString() + "m")
+              ],
+            ),
+          ),
         ),
       ),
     );
+  }
+
+  void _handleSubmitted() {
+    _formKey.currentState.save();
+
+    // TODO
+    print("startLatitude: " + startLatitude);
+    print("startLongitude: " + startLongitude);
+    print("endLatitude: " + endLatitude);
+    print("endLongitude: " + endLongitude);
+
+    setState(() {
+      distance += 1;
+    });
   }
 }
