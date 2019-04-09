fa2fdeb321758d9680b4187754874d353bf32370
noboru-i <ishikura.noboru@gmail.com>
2019-04-09 23:59:58 +0900

    impl android distance

--- android/src/main/kotlin/io/github/noborui/flutter_gps_distance/FlutterGpsDistancePlugin.kt
+++ android/src/main/kotlin/io/github/noborui/flutter_gps_distance/FlutterGpsDistancePlugin.kt
@@ -1,5 +1,6 @@
 package io.github.noborui.flutter_gps_distance
 
+import android.location.Location
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
@@ -16,8 +17,17 @@ class FlutterGpsDistancePlugin: MethodCallHandler {
   }
 
   override fun onMethodCall(call: MethodCall, result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
+    if (call.method == "calculateDistance") {
+      val args = call.arguments as List<Double>
+      print("start latitude : %f".format(args[0]));
+      print("start longitude : %f".format(args[1]));
+      print("end latitude : %f".format(args[2]));
+      print("end longitude : %f".format(args[3]));
+
+      val results = FloatArray(3)
+      Location.distanceBetween(args[0], args[1], args[2], args[3], results);
+      print("distance %f".format(results[0]))
+      result.success(results[0])
     } else {
       result.notImplemented()
     }
